35b8dbf57e39cb325deb86f1f68c342b6eca54c0
Karol Skalutki| <skalutki@gmail.com>
2019-07-08 13:51:10 +0100

    Feat: transfer project to gulp

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,23 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+const welcome = (name, age) => {
+    console.log(`Witaj ${name}, masz ${age} lat!`);
+}
+
+welcome("Karol", 30);
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+
+navigationSwitcher.addEventListener('click', (e) => {
+
+    const navigationList = document.querySelector('.navigation__list--js');
+    if (navigationList.classList.toggle('navigation__list--visible')) {
+        const navigationSwitcherX = document.querySelector('.navigation__switcherX--js')
+        navigationSwitcherX.innerHTML = '<button class="navigation__switcherX navigation__switcherX--js">X</button>';
+    } else {
+        console.log('po kliknięciu w przycisk otwierania menu ikona powinna się zmienić');
+    }
+});
