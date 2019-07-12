32fd17752c8da1cc9847408ced245ef22c0259e2
Karol Skalutki| <skalutki@gmail.com>
2019-07-12 16:27:53 +0100

    Feat: refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,23 @@
 "use strict";
 
-import moment from 'moment';
+import moment from "moment";
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 const welcome = (name, age) => {
-    console.log(`Witaj ${name}, masz ${age} lat!`);
-}
+  console.log(`Witaj ${name}, masz ${age} lat!`);
+};
 
 welcome("Karol", 30);
 
-const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+const navigationSwitcher = document.querySelector(".navigation__switcher--js");
 
-navigationSwitcher.addEventListener('click', (e) => {
-
-    const navigationList = document.querySelector('.navigation__list--js');
-    if (navigationList.classList.toggle('navigation__list--visible')) {
-        const navigationSwitcherX = document.querySelector('.navigation__switcherX--js')
-        navigationSwitcherX.innerHTML = '<button class="navigation__switcherX navigation__switcherX--js">X</button>';
-    } else {
-        console.log('po kliknięciu w przycisk otwierania menu ikona powinna się zmienić');
-    }
-});
+navigationSwitcher.addEventListener("click", e => {
+  const navigationList = document.querySelector(".navigation__list--js");
+  navigationList.classList.toggle("navigation__list--visible");
+  //if (navigationList.contains("navigation__list--visible")) {
+   // navigationSwitcher.innerHTML = "X";
+  //} else {
+  //  navigationSwitcher.innerHTML = "≡";
+  //}
+//});
