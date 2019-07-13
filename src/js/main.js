f0ad03c06ccffbb9c837f5367204a138287e9aec
Karol Skalutki| <skalutki@gmail.com>
2019-07-13 18:44:51 +0100

    feat: gh image

--- src/js/main.js
+++ src/js/main.js
@@ -10,15 +10,15 @@ const welcome = (name, age) => {
 
 welcome("Karol", 30);
 
-const navigationSwitcher = document.querySelector(".navigation__switcher--js");
+const navigationSwitcher = document.querySelector(".navigation__switcher--js")
 
-navigationSwitcher.addEventListener("click", e => {
-  const navigationList = document.querySelector(".navigation__list--js");
-  navigationList.classList.toggle("navigation__list--visible");
+navigationSwitcher.addEventListener('click', (e) => {
+
+  const navigationList = document.querySelector('.navigation__list--js');
+  if (navigationList.classList.toggle('navigation__list--visible')) {
+      const navigationSwitcherX = document.querySelector('.navigation__switcherX--js')
+      navigationSwitcherX.innerHTML = '<button class="navigation__switcherX navigation__switcherX--js">X</button>';
+  }else{
+      console.log('po kliknięciu w przycisk otwierania menu ikona powinna się zmienić');
+  }
 });
-//if (navigationList.contains("navigation__list--visible")) {
-// navigationSwitcher.innerHTML = "X";
-//} else {
-//  navigationSwitcher.innerHTML = "≡";
-//}
-//});
