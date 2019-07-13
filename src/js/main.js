3621c455cb588585fcc6ac6226553b72190ce6e4
Karol Skalutki| <skalutki@gmail.com>
2019-07-13 17:46:26 +0100

    Feat: whiteVersion

--- src/js/main.js
+++ src/js/main.js
@@ -15,9 +15,10 @@ const navigationSwitcher = document.querySelector(".navigation__switcher--js");
 navigationSwitcher.addEventListener("click", e => {
   const navigationList = document.querySelector(".navigation__list--js");
   navigationList.classList.toggle("navigation__list--visible");
-  //if (navigationList.contains("navigation__list--visible")) {
-   // navigationSwitcher.innerHTML = "X";
-  //} else {
-  //  navigationSwitcher.innerHTML = "≡";
-  //}
+});
+//if (navigationList.contains("navigation__list--visible")) {
+// navigationSwitcher.innerHTML = "X";
+//} else {
+//  navigationSwitcher.innerHTML = "≡";
+//}
 //});
